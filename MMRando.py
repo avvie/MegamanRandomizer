89b2eaf5605add2778b4beb9b0db02748bb6643a
JoJoCrusade <66278471+JoJoCrusade@users.noreply.github.com>
2023-01-19 14:09:46 -0500

    Added reference files refactored main script added options

--- MMRando.py
+++ MMRando.py
@@ -1,23 +1,27 @@
 import binascii
 import random
 import shutil
+import argparse
+import Data
+import Offsets
 
-#Original weapon bytes
-Vanilla_Weapons = [0x20, 0x10, 0x02, 0x40, 0x04, 0x08]
-print("Vanilla Weapons",Vanilla_Weapons)
 
-#The shuffled weapon bytes
-New_Weapons = []
-Megaman_Default = [0x2c, 0x11]
+settings = argparse.ArgumentParser()
 
-#to do: make palette file that python can load from longer list
-primaryPal_Megaman = [0x2c, 0x11, 0x30, 0x00, 0x30, 0x12, 0x30, 0x19, 0x28, 0x16, 0x38, 0x00, 0x30, 0x17, 0x2C, 0x11]
-print(primaryPal_Megaman)
+settings.add_argument("--skipweapons", help="Skips weapon randomization",  action="store_true", default=False)
+settings.add_argument("--skipcolors", help="Skip color randomization", action="store_true", default=False)
 
-Megaman_Default_Offsets = [0xCB1, 0xCE1, 0xDDD, 0x4CB1, 0x4CE1, 0X4D9B, 0x8CB1, 0x8CE1, 0xCCB1, 0xCCE1, 
-0x10CB1, 0x10ce1, 0x14cb1, 0x1D485, 0x11D493, 0x14CE1]#Megamans stage default palette values we need to overwrite
-                                                      #Dear god why are there so many
+args = settings.parse_args()
 
+##Byte writing testing
+filecopy = shutil.copyfile("./Mega Man (USA).nes", "./MMRando.nes")
+
+file = open("MMRando.nes", "r+b")
+
+def patcher(offset, data):
+    file.seek(offset)
+    file.write(int.to_bytes(data))
+    
 def paletteGenerator(color1, color2):
         
     print("palette", color1, color2)
@@ -38,60 +42,31 @@ def paletteGenerator(color1, color2):
         
     
     rando_palette = [new_paletteh, new_palettel]
-    return rando_palette
+    return rando_palette    
+
+if args.skipweapons == False:
+    Weapons = Data.Vanilla_Weapons
+    random.shuffle(Weapons)
+    for x in range(5):
+        patcher(Offsets.WeaponReward + x, Weapons[x])
+        patcher(Offsets.BossDefeated + x, Weapons[x])
+    patcher(Offsets.Gutsman_ClearPatch, Weapons[5])
+    print("randomized weapons", Weapons)
     
-#Generate Megaman Default Palette
-Megaman_Default_New = paletteGenerator(0x2c, 0x11)
-
-
-#byte shuffle loop
-while (len(Vanilla_Weapons) > 0):
-    length = len(Vanilla_Weapons)
-    r = random.randrange(length)
-    print("rng",r)
-    newByte = Vanilla_Weapons[r]
-    New_Weapons.append(newByte)
-    Vanilla_Weapons.pop(r)
-  
+if args.skipcolors == False:
+    Default_Pal = paletteGenerator(0x2c, 0x11)
+    for x in Offsets.Megaman_DefaultPal:
+        patcher(x, Default_Pal[0])
+        file.write(int.to_bytes(Default_Pal[1])) #hack for writing two values based off one offset. TO DO: Expand patcher for support
     
-# print("Weapons1",Vanilla_Weapons)
-print("Randomized Weapons",New_Weapons)
-
-##Byte writing testing
-filecopy = shutil.copyfile("./Mega Man (USA).nes", "./MMRando.nes")
-
-file = open("MMRando.nes", "r+b")
-
-file.seek(0x1C148) #weapons offset
-for x in New_Weapons:
-    file.write(int.to_bytes(x))
-#####fixes incorrect level beaten graphics on stage select
-file.seek(0x1BFCC) #boss defeated offset
-for x in New_Weapons:
-    file.write(int.to_bytes(x))
+    for x in range(2, 7, 2):
+        Weapon_Pal = paletteGenerator(Data.primaryPal_Megaman[x], Data.primaryPal_Megaman[x+1])
+        patcher(Offsets.Megaman_WeaponPal + x, Weapon_Pal[0])
+        file.write(int.to_bytes(Weapon_Pal[1])) #hack for writing two values based off one offset. TO DO: Expand patcher for support
     
-file.seek(0x1B69E) #Gutsman drawn sprite works differently on the level select
-file.write(int.to_bytes(New_Weapons[5])) #This patches a CMP call of 08 (Gutsman's usual reward) with the new reward assigned to GM
+    patcher(Offsets.Bossroom_PalPatch[0], Default_Pal[0])
+    patcher(Offsets.Bossroom_PalPatch[1], Default_Pal[1])
 
-###Fixed MegaMans sprite reverting to 2c 11 when dying in boss rooms
-file.seek(0x1C29B)
-file.write(int.to_bytes(Megaman_Default_New[0])) #replaces a lda #$2c with our new palette
-file.seek(0x1C2A0)
-file.write(int.to_bytes(Megaman_Default_New[1])) #replaces a lda #$11 with our new palette
-
-for offset in Megaman_Default_Offsets: #Writes our megaman palette to palette tables
-    file.seek(offset)
-    file.write(int.to_bytes(Megaman_Default_New[0]))
-    file.write(int.to_bytes(Megaman_Default_New[1]))
-
-file.seek(0x1D487)#MM palette offset
-x = 2 #hack to skip the buster palette we just wrote
-while x < 15: 
-    Generated_Palettes = paletteGenerator(primaryPal_Megaman[x],primaryPal_Megaman[(x+1)])
-    file.write(int.to_bytes(Generated_Palettes[0]))
-    file.write(int.to_bytes(Generated_Palettes[1]))
-    print("Writing Palettes ",Generated_Palettes)
-    x += 2
 
 file.close()
 
--- Offsets.py
+++ Offsets.py
@@ -1,13 +1,14 @@
 #Instances of loading Mega Man's classic blue palette
-Megaman_DefaultPal_Offsets = [0xCB1, 0xCE1, 0xDDD, 0x4CB1, 0x4CE1, 0X4D9B, 0x8CB1, 0x8CE1, 0xCCB1, 0xCCE1, 
+Megaman_DefaultPal = [0xCB1, 0xCE1, 0xDDD, 0x4CB1, 0x4CE1, 0X4D9B, 0x8CB1, 0x8CE1, 0xCCB1, 0xCCE1, 
 0x10CB1, 0x10ce1, 0x14cb1, 0x1D485, 0x11D493, 0x14CE1]
 
-Megaman_WeaponPal_Offset = 0x1D487 # 2 bytes for each color with 7 weapon colors C I B F E G M
+Megaman_WeaponPal = 0x1D487 # 2 bytes for each color with 7 weapon colors C I B F E G M
 
 WeaponReward = 0x1C148 #Table for rewarding weapons
-BossDefeated_Offset = 0x1BFCC  #Table for bosses deafeted
+BossDefeated = 0x1BFCC  #Table for bosses deafeted
 
 Gutsman_ClearPatch = 0x1B69E # Fixes a cmp call for checking if we beat gutsman
 
-Megaman_Bossroom_PalPatch = [0x1C29B, 0x1C2A0] # Patches hard coded palette values
+Bossroom_PalPatch = [0x1C29B, 0x1C2A0] # Patches hard coded palette values
                                                 # First address is 2c, the second is 11
+                                                
