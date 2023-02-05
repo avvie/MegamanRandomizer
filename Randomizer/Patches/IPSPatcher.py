831153bfd0c44d9c728be22d8a493a664b59e28a
JoJoCrusade <66278471+JoJoCrusade@users.noreply.github.com>
2023-02-04 23:54:17 -0500

    Removed testing stuff

--- Randomizer/Patches/IPSPatcher.py
+++ Randomizer/Patches/IPSPatcher.py
@@ -22,23 +22,16 @@ class IPSPatcher(PatchBase):
         while End_of_file_Checker != bytes(b'EOF'):
             Record = bytes(self.IPSFile.read(3))
             End_of_file_Checker = Record
-            Offset = int.from_bytes(Record, 'big')
-            # print("Read ROM offset " , Offset, "at patch offset ", PatchOffset)
-            # PatchOffset = PatchOffset+3 #we read 3 bytes and move the offset accordingly
-        
+            Offset = int.from_bytes(Record, 'big')       
             Size = int.from_bytes(self.IPSFile.read(2))
-            # PatchOffset = (PatchOffset+2)
 
         if Size != 0:           
             PatchBytes = self.IPSFile.read(Size)
             self.file.seek(Offset)
             self.file.write(PatchBytes)
         else: 
-            # print("RLE patch")
             RLE_Size = int.from_bytes(self.IPSFile.read(2))
             Value = self.IPSFile.read(1)
-            # PatchOffset = PatchOffset + 3
-            # print("RLE Size ", RLE_Size, "Byte to write ", Value, "at offset ", PatchOffset)
             self.file.seek(Offset)
             while RLE_Size > 0:
                 self.file.write(Value)
