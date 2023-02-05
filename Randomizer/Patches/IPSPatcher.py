658801c6f1ae9c9462e2d3dce58e3567a410baf4
JoJoCrusade <66278471+JoJoCrusade@users.noreply.github.com>
2023-02-04 21:23:50 -0500

    Renamed files/folders

--- Randomizer/Patches/IPSPatcher.py
+++ Randomizer/Patches/IPSPatcher.py
@@ -1,7 +1,6 @@
 from BaseClasses.PatchBase import *
 
-class IPS(PatchBase):
-    PatchFile = open(file("./IPS/ROLLCHAN.ips"))
+class IPSPatcher(PatchBase):
     
     def __init__(self, file, params = None):
         super().__init__(file, params)
@@ -10,37 +9,34 @@ class IPS(PatchBase):
         super().Patch()
         self.__Write()
         
-    def __Write(self):
+    def __Write(self, IPSFile):
     Offset = 0
     Size = 0
     RLE_Size = 0
     Value = 0
     End_of_file_Checker = bytes(b'/x00')
     x = 0
-    PatchFile.seek(5)
+    IPSFile.seek(5) #Skip past header
     while End_of_file_Checker != bytes(b'EOF'):
         # print("Patch offset: " ,PatchOffset)
-        # PatchFile.seek(PatchOffset)
-        Record = bytes(PatchFile.read(3))
+        # IPSFile.seek(PatchOffset)
+        Record = bytes(IPSFile.read(3))
         End_of_file_Checker = Record
         Offset = int.from_bytes(Record, 'big')
         # print("Read ROM offset " , Offset, "at patch offset ", PatchOffset)
         # PatchOffset = PatchOffset+3 #we read 3 bytes and move the offset accordingly
     
-        Size = int.from_bytes(PatchFile.read(2))
+        Size = int.from_bytes(IPSFile.read(2))
         # PatchOffset = (PatchOffset+2)
 
-        if Size != 0:
-            if Size == 1:
-                print("Sketchy patch at patchoffset ", PatchOffset, "writing to ", Offset, " Offset")
-            # PatchOffset = PatchOffset + Size #read 2 bytes and move the offset accordingly then move forward how many bytes we've written
-            PatchBytes = PatchFile.read(Size)
+        if Size != 0:           
+            PatchBytes = IPSFile.read(Size)
             RomFile.seek(Offset)
             RomFile.write(PatchBytes)
-        else:
+        else: 
             # print("RLE patch")
-            RLE_Size = int.from_bytes(PatchFile.read(2))
-            Value = PatchFile.read(1)
+            RLE_Size = int.from_bytes(IPSFile.read(2))
+            Value = IPSFile.read(1)
             # PatchOffset = PatchOffset + 3
             # print("RLE Size ", RLE_Size, "Byte to write ", Value, "at offset ", PatchOffset)
             RomFile.seek(Offset)
