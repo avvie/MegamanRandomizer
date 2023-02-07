8847deddf894d0f95794350bca623f2c11ef7b7a
JoJoCrusade <66278471+JoJoCrusade@users.noreply.github.com>
2023-02-07 16:55:48 -0500

    Tiny style and comment changes

--- Randomizer/Patches/IPSPatcher.py
+++ Randomizer/Patches/IPSPatcher.py
@@ -13,17 +13,17 @@ class IPSPatcher(PatchBase):
     
     def __Write(self):
         record = bytes()
-        self.ips_file.seek(5) #skip past PATCH string header in the ips file
-        while record != bytes(b'EOF'): #Checks for the EOF (end of file) string that ends an ips file
+        self.ips_file.seek(5) #skip past "PATCH" string header in the ips file
+        while record != bytes(b'EOF'): #Checks for string "EOF" (end of file) that ends an ips file
             record = bytes(self.ips_file.read(3))
             offset = (int.from_bytes(record, 'big')) - 0x10 #Subtracting the header size
             size = int.from_bytes(self.ips_file.read(2))
-
 
             if size != 0:           
                 patchbytes = self.ips_file.read(size)
                 self.file.seek(offset)
                 self.file.write(patchbytes)
+                
             else: 
                 rle_size = int.from_bytes(self.ips_file.read(2))
                 value = self.ips_file.read(1)
@@ -31,6 +31,7 @@ class IPSPatcher(PatchBase):
                 while rle_size > 0:
                     self.file.write(value)
                     rle_size = rle_size - 1
+                    
         self.ips_file.close()
 
     def Patch(self):
