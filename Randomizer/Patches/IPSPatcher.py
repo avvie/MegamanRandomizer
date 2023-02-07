fd2976c19a6c4c80ba5493176a686a257f1dcb77
JoJoCrusade <66278471+JoJoCrusade@users.noreply.github.com>
2023-02-07 16:50:17 -0500

    removed unused variable, more consistant syling

--- Randomizer/Patches/IPSPatcher.py
+++ Randomizer/Patches/IPSPatcher.py
@@ -7,32 +7,31 @@ class IPSPatcher(PatchBase):
         super().__init__(file, params)        
         
     def __GetFile(self):
-        self.IPS_Folder = Path("IPSFiles/")
-        self.PatchFile = self.IPS_Folder / self.params
-        self.IPSFile = open(self.PatchFile, "r+b")
+        self.ips_folder = Path("IPSFiles/")
+        self.patch_file = self.ips_folder / self.params
+        self.ips_file = open(self.patch_file, "r+b")
     
     def __Write(self):
         record = bytes()
-        self.IPSFile.seek(5) #skip past PATCH string header in the ips file
+        self.ips_file.seek(5) #skip past PATCH string header in the ips file
         while record != bytes(b'EOF'): #Checks for the EOF (end of file) string that ends an ips file
-            record = bytes(self.IPSFile.read(3))
-            end_of_file_checker = record
+            record = bytes(self.ips_file.read(3))
             offset = (int.from_bytes(record, 'big')) - 0x10 #Subtracting the header size
-            size = int.from_bytes(self.IPSFile.read(2))
+            size = int.from_bytes(self.ips_file.read(2))
 
 
             if size != 0:           
-                PatchBytes = self.IPSFile.read(size)
+                patchbytes = self.ips_file.read(size)
                 self.file.seek(offset)
-                self.file.write(PatchBytes)
+                self.file.write(patchbytes)
             else: 
-                rle_size = int.from_bytes(self.IPSFile.read(2))
-                value = self.IPSFile.read(1)
+                rle_size = int.from_bytes(self.ips_file.read(2))
+                value = self.ips_file.read(1)
                 self.file.seek(offset)
                 while rle_size > 0:
                     self.file.write(value)
                     rle_size = rle_size - 1
-        self.IPSFile.close()
+        self.ips_file.close()
 
     def Patch(self):
         super().Patch()
