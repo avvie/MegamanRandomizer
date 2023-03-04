48f218481e944828b8f7654e388d7453a323aa4b
JoJoCrusade <66278471+JoJoCrusade@users.noreply.github.com>
2023-03-04 00:45:37 -0500

    Refactored Weakness Generator

--- Randomizer/Generators/WeaknessGenerator.py
+++ Randomizer/Generators/WeaknessGenerator.py
@@ -2,67 +2,78 @@ from BaseClasses.GeneratorBase import *
 
 
 class WeaknessGenerator(GeneratorBase):
-    WeaknessByteIndex = [1, 2, 3, 4, 5, 6]
+    #                    C  I  B  F  E  G
+    BossWeaponIndexes = [1, 2, 3, 4, 5, 6]
     DamageTableOffset = 0x1FDEE
-    # P C I B F E G M
-    DamageChart = [1, 0, 0, 0, 0, 0, 0, 0]  # preset default values
-    DamageLists = []
+    # Corresponding weapon for each index: The value determines how much damage that weapon deals
+    # Damage values of    P  C  I  B  F  E  G  M
+    DefaultDamageChart = [1, 0, 0, 0, 0, 0, 0, 0]  # Preset default damage values
+    DamageCharts = []  # Damage charts that ultimately get written to rom
 
     def __init__(self, file, params = None):
         super().__init__(file, params)
 
+    # Generates a 6 x 8 byte long damage chart for the main Robot bosses.
+    # Each boss has an 8 byte list. Each index corresponds to a weapon, with the value being how much damage it does.
+    # Major weakness is simply a nickname for the most damaging weapon to a particular boss (typically 4 damage)
+    # Minor weakness is simply a nickname for the second most damaging weapon to a particular boss (typically 2 damage)
+    # Each boss' damage chart should have a major weakness unique to that boss, and a minor weakness
+    # The minor weakness should just be different from the major weakness
     def __Generate(self):
-        while len(self.WeaknessByteIndex) > 0:  # Counting down to 0 as we remove options frm the list
-            currentchart = self.DamageChart[:]
-            length = len(self.WeaknessByteIndex)
+        # Choosing a random boss weapon for the major weakness
+        while len(self.BossWeaponIndexes) > 0:  # Counting down to 0 as we remove options from the list
+            newdamagechart = self.DefaultDamageChart[:]
+            remainingoptions = len(self.BossWeaponIndexes)
 
-            # Get the byte offset that we're going to set to 4
-            r = random.randrange(length)  # From the list of remaining values
-            majorweakness = self.WeaknessByteIndex[r]  # Get the offset we'll set to 4
+            # Get the index of the random weapon we're going to assign 4 damage to
+            randomweaponindex = random.randrange(remainingoptions)
+            majorweaknessindex = self.BossWeaponIndexes[randomweaponindex]
 
-            currentchart[majorweakness] = 4
-            self.WeaknessByteIndex.pop(r)  # Remove that as a major weakness so each robot is unique
+            newdamagechart[majorweaknessindex] = 4
+            self.BossWeaponIndexes.pop(randomweaponindex)  # Remove for each robot to have a unique major weakness
 
-            if len(self.WeaknessByteIndex) > 0:
-                r = random.randrange(len(self.WeaknessByteIndex))
-                minorweakness = self.WeaknessByteIndex[r]
-                currentchart[minorweakness] = 2
-            else:
-                currentchart[0] = 3  # Gives a buster weakness if were out of boss weapons
-            self.DamageLists.append(currentchart)
+            # Choosing a boss weapon for the minor weakness
+            if len(self.BossWeaponIndexes) > 0:
+                randomweaponindex = random.randrange(len(self.BossWeaponIndexes))
+                minorweakness = self.BossWeaponIndexes[randomweaponindex]
+                newdamagechart[minorweakness] = 2
+            else:  # Give a minor weakness to buster if were out of boss weapons
+                newdamagechart[0] = 3  # Buster can't be a major weakness, let's give it a small buff in damage
+            self.DamageCharts.append(newdamagechart)
 
-    # Here we shuffle the list order - this shuffles it from the last boss to any random one
-    # Next we find where the Gutsman weakness is (it's the 7th byte in a chart)
-    # Then we swap the index with the Gutsman weakness with a randomly chosen boss that has throwable blocks
-
+    # For logic, we need to shuffle the order of lists to randomize the buster minor weakness
+    # Also only certain boss rooms have throwable blocks to utilize the weakness to Gutsman's weapon
+    # Logic sets the damage chart with a major weakness to Gutsman's weapon to a boss fight with throwable blocks
+    # Lastly, as there's only two throwable blocks - Buff Gutsman's weapon to deal 14 damage to kill a boss in two hits
     def __Logic(self):
-        random.shuffle(self.DamageLists)  # Shuffles the lists without shuffling the lists' contents
+        random.shuffle(self.DamageCharts)  # Shuffles the order of lists without shuffling the lists' contents
 
         original_index = 0
-        for damagevalue in self.DamageLists:
-            # Checks if the major weakness in assigned to Gutsman
-            if damagevalue[6] > 3:  # Has a major weakness to Gutsman
+        for chart in self.DamageCharts:
+            # Check if Gutsman's weapon is the most damaging weapon in a chart
+            if chart[6] > 3:  # Damage chart has a major weakness to Gutsman's wepaon
+                chart[6] = 14  # As mentioned before, buff damage to defeat a boss in only two hits
                 new_index = random.choice([0, 4, 5])  # These are the boss rooms that have throwable blocks
 
-                # we want to swap the original list with Gutsman weakness (original_index)
-                # with the randomly chosen index of 0, 4, or 5 (Cut, Elec, or Guts)
-                list_with_gutsman_weakness = self.DamageLists[original_index]
-                list_to_swap = self.DamageLists[new_index]
+                # Swap the original damage chart with a Gutsman weakness (original_index)
+                # With a randomly chosen index of 0, 4, or 5 (Cut, Elec, or Guts. They all have throwable blocks)
+                damagechart_with_gutsmanweakness = self.DamageCharts[original_index]
+                damagechart_with_throwableblocks = self.DamageCharts[new_index]
 
-                # Swap the two lists
-                self.DamageLists[original_index] = list_to_swap
-                self.DamageLists[new_index] = list_with_gutsman_weakness
+                # Swap the two charts
+                self.DamageCharts[original_index] = damagechart_with_throwableblocks
+                self.DamageCharts[new_index] = damagechart_with_gutsmanweakness
 
                 break
             else:
                 original_index = original_index + 1
 
     def __Write(self):
-        print("Weakness table", self.DamageLists)
+        print("Weakness table", self.DamageCharts)
         self.file.seek(self.DamageTableOffset)
-        for damagelist in self.DamageLists:
-            for damagebytes in damagelist:
-                self.file.write(int.to_bytes(damagebytes))
+        for damagecharts in self.DamageCharts:
+            for damagevalues in damagecharts:
+                self.file.write(int.to_bytes(damagevalues))
 
     def Randomize(self):
         super().Randomize()
