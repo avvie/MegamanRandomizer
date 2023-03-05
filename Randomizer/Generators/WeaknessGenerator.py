db730e6b77341bbdc7aaa89fd710f59eddac6bc8
JoJoCrusade <66278471+JoJoCrusade@users.noreply.github.com>
2023-03-04 19:59:32 -0500

    Replaced DefaultDamageChart with a randomly generated chart
    Most weapons now have a 50/50 chance to deal at least 1 damage. Buster always does at least 1.
    
    In vanilla Ice is the only boss weapon to never deal damage outside of the FireMan fight where it's his major weakness. Currently unsure if Ice would behave weirdly to other bosses. I'll test this later for a possible change

--- Randomizer/Generators/WeaknessGenerator.py
+++ Randomizer/Generators/WeaknessGenerator.py
@@ -5,9 +5,11 @@ class WeaknessGenerator(GeneratorBase):
     #                    C  I  B  F  E  G
     BossWeaponIndexes = [1, 2, 3, 4, 5, 6]
     DamageTableOffset = 0x1FDEE
-    # Corresponding weapon for each index: The value determines how much damage that weapon deals
-    # Damage values of    P  C  I  B  F  E  G  M
-    DefaultDamageChart = [1, 0, 0, 0, 0, 0, 0, 0]  # Preset default damage values
+    # Each boss has its own Damage chart that is 8 bytes long. The index points to the corresponding weapon (PCIBFEGM)
+    # And the value is how much damage that weapon does.
+    # For Example: Cutman's vanilla damage chart
+    #  P  C  I  B  F  E  G  M
+    # [3, 1, 0, 2, 3, 1, 14 0]
     DamageCharts = []  # Damage charts that ultimately get written to rom
 
     def __init__(self, file, params = None):
@@ -15,14 +17,16 @@ class WeaknessGenerator(GeneratorBase):
 
     # Generates a 6 x 8 byte long damage chart for the main Robot bosses.
     # Each boss has an 8 byte list. Each index corresponds to a weapon, with the value being how much damage it does.
-    # Major weakness is simply a nickname for the most damaging weapon to a particular boss (typically 4 damage)
-    # Minor weakness is simply a nickname for the second most damaging weapon to a particular boss (typically 2 damage)
+    # For each chart, most weapons have a 50/50 chance to deal 0 or 1 damage (Buster is always set to 1)
+    # Then a weapon is randomly chosen to be the major weakness, and a second weapon is chosen to be the minor weakness
+    # Major weakness is simply a nickname for the most damaging weapon in a particular chart(typically 4 damage)
+    # Minor weakness is simply a nickname for the second most damaging weapon in a particular chart (typically 2 damage)
     # Each boss' damage chart should have a major weakness unique to that boss, and a minor weakness
     # The minor weakness should just be different from the major weakness
     def __Generate(self):
         # Choosing a random boss weapon for the major weakness
         while len(self.BossWeaponIndexes) > 0:  # Counting down to 0 as we remove options from the list
-            newdamagechart = self.DefaultDamageChart[:]
+            newdamagechart = self.__damagechartGenerator()
             remainingoptions = len(self.BossWeaponIndexes)
 
             # Get the index of the random weapon we're going to assign 4 damage to
@@ -74,6 +78,18 @@ class WeaknessGenerator(GeneratorBase):
         for damagecharts in self.DamageCharts:
             for damagevalues in damagecharts:
                 self.file.write(int.to_bytes(damagevalues))
+
+    @staticmethod
+    def __damagechartGenerator():
+        damagechart = []
+        for damageindex in range(8):
+            damagevalue = random.choice([0, 1])
+            damagechart.append(damagevalue)
+        # Hardcode Buster to always do 1 damage, Ice 0 Damage, and Magnet 0 damage
+        damagechart[0] = 1  # Buster
+        damagechart[4] = 0  # Ice always does 0 damage in Vanilla except to Fireman where it does 4
+        damagechart[7] = 0  # Magnet creates platforms and isn't intended to deal damage
+        return damagechart[:]
 
     def Randomize(self):
         super().Randomize()
