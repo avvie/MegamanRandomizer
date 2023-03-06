227eaa90766e362d229a23cec6f9117dfe8521d8
JoJoCrusade <66278471+JoJoCrusade@users.noreply.github.com>
2023-03-06 09:10:58 -0500

    Main 6 boss weakness randomization and logic for boss rewards (#29)
    * Added prototype weakness generation
    
    Things probably need to be renamed but the logic works well. Need to test
    the ROM file and make sure it's being written to correctly
    
    * Correctly writes bytes - removed debug prints
    
    One last logic related thing I want to add is a 50/50 chance for each non-weakness weapon to do 1 damage to the boss.
    
    * Removed unused variable
    
    * Fixed calling the logic function, removed leftover prints, updated comments
    
    * Started remaking of Weapon randomization
    
    * Logic for boss not dropping its own weakness
    
    forgive me for i have sinned
    
    * Small refactoring
    
    Less ambiguous names and removal of unneeded values
    
    * Refacotred weapon generator for clarity
    
    Removed all/nearly all references of the word weakness for clarity. Names better explain what they actually do. Also moved the logc function to the top with a comment explaining the desired logic
    
    * Small style changes
    
    * Refactored Weakness Generator
    
    * Replaced DefaultDamageChart with a randomly generated chart
    
    Most weapons now have a 50/50 chance to deal at least 1 damage. Buster always does at least 1.
    
    In vanilla Ice is the only boss weapon to never deal damage outside of the FireMan fight where it's his major weakness. Currently unsure if Ice would behave weirdly to other bosses. I'll test this later for a possible change
    
    * Removed debug prints, added documentation for merging

---
+++ Randomizer/Generators/WeaknessGenerator.py
@@ -0,0 +1,97 @@
+from BaseClasses.GeneratorBase import *
+
+
+class WeaknessGenerator(GeneratorBase):
+    #                    C  I  B  F  E  G
+    BossWeaponIndexes = [1, 2, 3, 4, 5, 6]
+    DamageTableOffset = 0x1FDEE
+    # Each boss has its own Damage chart that is 8 bytes long. The index points to the corresponding weapon (PCIBFEGM)
+    # And the value is how much damage that weapon does.
+    # For Example: Cutman's vanilla damage chart
+    #  P  C  I  B  F  E  G  M
+    # [3, 1, 0, 2, 3, 1, 14 0]
+    DamageCharts = []  # Damage charts that ultimately get written to rom
+
+    def __init__(self, file, params = None):
+        super().__init__(file, params)
+
+    # Generates a 6 x 8 byte long damage chart for the main Robot bosses.
+    # Each boss has an 8 byte list. Each index corresponds to a weapon, with the value being how much damage it does.
+    # For each chart, most weapons have a 50/50 chance to deal 0 or 1 damage (Buster is always set to 1)
+    # Then a weapon is randomly chosen to be the major weakness, and a second weapon is chosen to be the minor weakness
+    # Major weakness is simply a nickname for the most damaging weapon in a particular chart(typically 4 damage)
+    # Minor weakness is simply a nickname for the second most damaging weapon in a particular chart (typically 2 damage)
+    # Each boss' damage chart should have a major weakness unique to that boss, and a minor weakness
+    # The minor weakness should just be different from the major weakness
+    def __Generate(self):
+        # Choosing a random boss weapon for the major weakness
+        while len(self.BossWeaponIndexes) > 0:  # Counting down to 0 as we remove options from the list
+            newdamagechart = self.__damagechartGenerator()
+            remainingoptions = len(self.BossWeaponIndexes)
+
+            # Get the index of the random weapon we're going to assign 4 damage to
+            randomweaponindex = random.randrange(remainingoptions)
+            majorweaknessindex = self.BossWeaponIndexes[randomweaponindex]
+
+            newdamagechart[majorweaknessindex] = 4
+            self.BossWeaponIndexes.pop(randomweaponindex)  # Remove for each robot to have a unique major weakness
+
+            # Choosing a boss weapon for the minor weakness
+            if len(self.BossWeaponIndexes) > 0:
+                randomweaponindex = random.randrange(len(self.BossWeaponIndexes))
+                minorweakness = self.BossWeaponIndexes[randomweaponindex]
+                newdamagechart[minorweakness] = 2
+            else:  # Give a minor weakness to buster if were out of boss weapons
+                newdamagechart[0] = 3  # Buster can't be a major weakness, let's give it a small buff in damage
+            self.DamageCharts.append(newdamagechart)
+
+    # For logic, we need to shuffle the order of lists to randomize the buster minor weakness
+    # Also only certain boss rooms have throwable blocks to utilize the weakness to Gutsman's weapon
+    # Logic sets the damage chart with a major weakness to Gutsman's weapon to a boss fight with throwable blocks
+    # Lastly, as there's only two throwable blocks - Buff Gutsman's weapon to deal 14 damage to kill a boss in two hits
+    def __Logic(self):
+        random.shuffle(self.DamageCharts)  # Shuffles the order of lists without shuffling the lists' contents
+
+        original_index = 0
+        for chart in self.DamageCharts:
+            # Check if Gutsman's weapon is the most damaging weapon in a chart
+            if chart[6] > 3:  # Damage chart has a major weakness to Gutsman's wepaon
+                chart[6] = 14  # As mentioned before, buff damage to defeat a boss in only two hits
+                new_index = random.choice([0, 4, 5])  # These are the boss rooms that have throwable blocks
+
+                # Swap the original damage chart with a Gutsman weakness (original_index)
+                # With a randomly chosen index of 0, 4, or 5 (Cut, Elec, or Guts. They all have throwable blocks)
+                damagechart_with_gutsmanweakness = self.DamageCharts[original_index]
+                damagechart_with_throwableblocks = self.DamageCharts[new_index]
+
+                # Swap the two charts
+                self.DamageCharts[original_index] = damagechart_with_throwableblocks
+                self.DamageCharts[new_index] = damagechart_with_gutsmanweakness
+
+                break
+            else:
+                original_index = original_index + 1
+
+    def __Write(self):
+        self.file.seek(self.DamageTableOffset)
+        for damagecharts in self.DamageCharts:
+            for damagevalues in damagecharts:
+                self.file.write(int.to_bytes(damagevalues))
+
+    @staticmethod
+    def __damagechartGenerator():
+        damagechart = []
+        for damageindex in range(8):
+            damagevalue = random.choice([0, 1])
+            damagechart.append(damagevalue)
+        # Hardcode Buster to always do 1 damage, Ice 0 Damage, and Magnet 0 damage
+        damagechart[0] = 1  # Buster
+        damagechart[4] = 0  # Ice always does 0 damage in Vanilla except to Fireman where it does 4
+        damagechart[7] = 0  # Magnet creates platforms and isn't intended to deal damage
+        return damagechart[:]
+
+    def Randomize(self):
+        super().Randomize()
+        self.__Generate()
+        self.__Logic()
+        self.__Write()
--- Randomizer/Generators/WeaponGenerator.py
+++ Randomizer/Generators/WeaponGenerator.py
@@ -1,40 +1,86 @@
 from BaseClasses.GeneratorBase import *
 
+
 class WeaponGenerator(GeneratorBase):
-    #Original weapon bytes
-    Vanilla_Weapons = [0x20, 0x10, 0x02, 0x40, 0x04, 0x08]
-    Weapons_Write_Offset = 0x1C148
-    Boss_Defeated_Weapon_Write_Offset = 0x1BFCC
-    Gutsman_Specific_Fix_Write_Offset = 0x1B69E
+    # Original weapon reward bytes
+    # Cut, Ice, Bomb, Fire, Elec, Guts
+    Weapon_Rewards = [0x20, 0x10, 0x02, 0x40, 0x04, 0x08]
+    Rewards_Table_Offset = 0x1C148
+    Boss_Defeated_Table_Offset = 0x1BFCC
+    Gutsman_Specific_Fix_Offset = 0x1B69E
+    Damage_Table_Offset = 0x1FDEE
+    DamageLists = []
+    Rewards_Table = []
+    # This may be dumb to use
+    # Converts index of most damaging weapon to the corresponding weapon reward
+    DamageIndex_to_Reward_Dict = {1: 0x20, 2: 0x10, 3: 0x02, 4: 0x40, 5: 0x04, 6: 0x08}
 
     def __init__(self, file, params = None):
         super().__init__(file, params)
 
-    def ShuffleWeapons(self):
-        New_Weapons = []
-        #byte shuffle loop
-        while (len(self.Vanilla_Weapons) > 0):
-            length = len(self.Vanilla_Weapons)
-            r = random.randrange(length)
-            newByte = self.Vanilla_Weapons[r]
-            New_Weapons.append(newByte)
-            self.Vanilla_Weapons.pop(r)
-        return New_Weapons
+    def __Logic(self):
+        # For each boss damage list in the Damage Chart
+        # Find the index of the weapon that does the most damage to that boss
+        # Exclude the most damaging weapon as a drop reward for that boss
+        # Then give a random reward from valid remaining options
+        for damagelist in self.DamageLists:
+            rewardlist = self.Weapon_Rewards[:]  # A copy makes it easier to temporarily remove a reward
+
+            sorted_damagelist = damagelist[:]
+            sorted_damagelist.sort()  # sets the highest damage value to the last (7th) index
+
+            # Weakness is just a nickname for the weapon that does the most damage
+            # Find the highest value's index from the original damage list
+
+            excluded_index = damagelist.index(sorted_damagelist[7])
+            # Convert from damage index to weapon reward
+            excluded_award = self.DamageIndex_to_Reward_Dict[excluded_index]
+
+            # Remove the excluded reward as an option from the temporary list
+            # The reward may have already been assigned to another boss - use try to see
+            try:
+                rewardlist.pop(rewardlist.index(excluded_award))
+            except ValueError:
+                pass  # already removed
+
+            # Assign a random reward from remaining options
+            try:
+                reward = random.choice(rewardlist)
+            except IndexError:  # Last reward available has been excluded
+                # Let's just swap the excluded reward with the previous one
+                prev_reward = self.Rewards_Table[4]
+                self.Rewards_Table[4] = excluded_award
+                self.Rewards_Table.append(prev_reward)
+            else:
+                # Remove chosen reward from future options
+                self.Weapon_Rewards.pop(self.Weapon_Rewards.index(reward))
+                self.Rewards_Table.append(reward)
 
     def __Generate(self):
-        self.shuffled_weapons = self.ShuffleWeapons()
+        self.__ReadDamageChart()
+        self.__Logic()
 
     def __Write(self):
-        self.file.seek(self.Weapons_Write_Offset) #weapons offset
-        for x in self.shuffled_weapons:
-            self.file.write(int.to_bytes(x))
-        #####fixes incorrect level beaten graphics on stage select
-        self.file.seek(self.Boss_Defeated_Weapon_Write_Offset) #boss defeated offset
-        for x in self.shuffled_weapons:
-            self.file.write(int.to_bytes(x))
+        self.file.seek(self.Rewards_Table_Offset)
+        for reward in self.Rewards_Table:
+            self.file.write(int.to_bytes(reward))
 
-        self.file.seek(self.Gutsman_Specific_Fix_Write_Offset) #Gutsman drawn sprite works differently on the level select
-        self.file.write(int.to_bytes(self.shuffled_weapons[5])) #This patches a CMP call of 08 (Gutsman's usual reward) with the new reward assigned to GM
+        self.file.seek(self.Boss_Defeated_Table_Offset)
+        for reward in self.Rewards_Table:
+            self.file.write(int.to_bytes(reward))
+
+        self.file.seek(self.Gutsman_Specific_Fix_Offset)  # Gutsman drawn sprite works differently on the level select
+        self.file.write(int.to_bytes(self.Rewards_Table[5]))
+
+    def __ReadDamageChart(self):
+        # Get the 6 x 8 damage chart
+        self.file.seek(self.Damage_Table_Offset)
+        for damagelists in range(6):
+            damagelist = []
+            for damagevalues in range(8):
+                damage = int.from_bytes(self.file.read(1))
+                damagelist.append(damage)
+            self.DamageLists.append(damagelist)
 
     def Randomize(self):
         super().Randomize()
